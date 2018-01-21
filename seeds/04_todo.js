2c99fab5eb5bcb3f47f30b4208d9b40f0b064a52
bethjaw <jaworskibeth@gmail.com>
2018-01-20 21:53:26 -0700

    updated ids

--- seeds/04_todo.js
+++ seeds/04_todo.js
@@ -9,31 +9,31 @@ exports.seed = function(knex, Promise) {
           // id: 1,
           title: 'The Iceline Trail (YOHO National Park Hike)',
           details: 'We were basically alone on this trail. It started in the woods and went up to rocky, lake filled mountain top with awesome views! Probably around 11 miles total, so we packed lunch and ate along the way. This was my favorite part of the trip!',
-          location_id: 1,
+          location_id: 100,
         },
         {
           // id: 2,
           title: 'Johnston Canyon Hike',
           details: 'This was also really cool. A little more crowded, but you can see some clear hot spring pools. Kinda of a nice half day hike that you can make into whatever you want!',
-          location_id: 1,
+          location_id: 100,
         },
         {
           // id: 3,
           title: 'Pyramid, Moraine and Malgine Lakes',
           details: 'Pyramid Has the most perfect views each morning! Great place to go to relax!  Moraine is close to Lake Louise and not as crowded (yay!) - It was worth seeing for me! Malgine is also very beautiful. We did some trail running here!',
-          location_id: 1,
+          location_id: 100,
         },
         {
           // id: 4,
           title: 'Emerald Lake & Natural Bridge',
           details: 'We did this while passing through, cool to see but not the best when comparing to everything else.',
-          location_id: 1,
+          location_id: 100,
         },
         {
           // id: 5,
           title: 'Wine Country, BC',
           details: 'This was a game changer for us!! We had no idea BC was wine country to the max. There are some AMAZING small winery\'s that all do samplings and tastings. If you love wine, this is definitely something you should add to the trip. (It was so beautiful too!)',
-          location_id: 1,
+          location_id: 100,
         },
       ]);
     });
--- seeds/05_genrecs.js
+++ seeds/05_genrecs.js
@@ -9,25 +9,25 @@ exports.seed = function(knex, Promise) {
           // id: 1,
           tip: 'Western Canada (BANFF & Jasper) is the place to go in the summer...',
           advice: 'It is full of selfie-stick tour bus tourists in certain places, but also has the amazing remote locations you feel you\'ve lone discovered.',
-          location_id: 1,
+          location_id: 100,
         },
         {
           // id: 2,
           tip: 'Go to places early or late. ',
           advice: 'We went to Lake Louise in the afternoon and it was packed, and honestly disappointing. However we got up early the next morning and were the 1st ones there. Saw the mist off the lake, quiet and BEAUTIFUL!',
-          location_id: 1,
+          location_id: 100,
         },
         {
           // id: 3,
           tip: 'Carry Bear Spray',
           advice: 'Bears were everywhere and super active. We picked up spray once we arrived because we couldn\'t carry it over the border.',
-          location_id: 1,
+          location_id: 100,
         },
         {
           // id: 4,
           tip: 'Use the park rangers and their info',
           advice: 'They are the BEST. We got so much information on good hikes that wouldn\'t be crowded, or if we wanted a long or short hike they were there to help!',
-          location_id: 1,
+          location_id: 100,
         },
       ]);
     });
